62878dd95db9c4d1619096d02487c6d5e54a0b8f
manipalreddy1747 <manipalreddy1747@gmail.com>
2023-04-03 08:40:26 +0530

    lingampalli_3april2023

---
+++ src/main/java/com/hyderabad/Lingampalli/Controller/Lingampalli.java
@@ -0,0 +1,36 @@
+package com.hyderabad.Lingampalli.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/home")
+public class Lingampalli {
+	
+	@GetMapping("/flatsize")
+	public String getFlatSize() {
+		return "480 Square Feet";
+	}
+	
+	@GetMapping("/bedrooms")
+	public String getBedrooms() {
+		return "1 Bedroom";
+	}
+	
+	@GetMapping("/washrooms")
+	public String getWashrooms() {
+		return "1";
+	}
+	
+	@GetMapping("/facing")
+	public String getFacing() {
+		return "South";
+	}
+	
+	@GetMapping("/uds")
+	public String getUndividedShare() {
+		return "11 Square Yards";
+	}
+
+}
---
+++ src/test/java/com/hyderabad/Madeenaguda/MadeenagudaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hyderabad.Madeenaguda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MadeenagudaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
